aa0fa87ef1baa314e6800070c48b409cb2a5db65
Colin Jacobs <colin@coljac.net>
2020-12-08 16:08:42 +1100

    Fixed up. No stamp duty

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,21 +34,42 @@ def d(r, n):
 
 @st.cache
 def make_figure(results):
-    # fig = go.Figure()
     fig = make_subplots(specs=[[{"secondary_y": True}]])
+
     fig.add_trace(go.Scatter(x=[z['month']/12 for z in results], 
                             y=[z['principle'] for z in results],
+                    hovertemplate = '%{x:.1f} years<br>\nPrinciple: %{y:$,.0f}<extra></extra>',
                     mode='lines+markers',
-                    name='principle remaining'))
+                    name='Principle remaining'))
     fig.add_trace(go.Scatter(x=[z['month']/12 for z in results], 
                             y=[z['interest_paid'] for z in results],
+                    hovertemplate = '%{x:.1f} years<br>\nInterest: %{y:$,.0f}<extra></extra>',
                     mode='lines+markers',
-                    name='interest paid'))
-
+                    name='Interest paid'))
     fig.add_trace(go.Scatter(x=[z['month']/12 for z in results], 
                             y=[z['offset'] for z in results],
                     mode='lines+markers',
-                    name='offset'))
+                    hovertemplate = '%{x:.1f} years<br>\nOffset: %{y:$,.0f}<extra></extra>',
+                    name='Offset account balance'))
+
+    fig.update_layout(
+        width=1100, height=600,
+                xaxis_title="Years",
+    yaxis_title="Loan value",
+    font = {"size": 14},
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.6
+    ),
+    # hovermode="x unified" ,
+        # font_family="Courier New",
+        # font_color="blue",
+        # title_font_family="Times New Roman",
+        # title_font_color="red",
+        # legend_title_font_color="green"
+    )
 
     return fig
 
@@ -61,7 +82,6 @@ def scenario(rate, principle, payment, cash_left=0, offset_average=0):
         i = loan.make_payment(payment)
         results.append([loan.principle, loan.interest_paid, i])
         if loan.principle <= 0:
-            print(f"Paid off in {m/12:.1f} years. Total interest paid: ${loan.interest_paid:.0f}")
             break    
             
 
@@ -79,46 +99,54 @@ def scenario2(rate, principle, income=0, start_cash=0, expenses=2500, reserve=15
             break    
     return results
 
+def calc_stamp_duty(state, house_cost):
+    duty = 0
+    message = ""
+    if state == "Vic":
+        if house_cost < 1000000:
+            duty = int(0.75 * (0.055 * house_cost))
+            message = "discount applied"
+        else:
+            duty = int((0.055 * house_cost))
+    elif state == "NSW":
+        if house_cost < 1033000:
+            duty = int(9285 + .045*(house_cost - 310000))
+        else:
+            duty = (41820 + .055*(house_cost - 1033000))
+
+    else:
+        duty = .04*house_cost
+        message = "Not real value"
+
+    return int(duty), message
+
 st.title('Mortgage calculator')
 
-house_cost = st.sidebar.slider('Cost of house', value=1000000, min_value=900000, max_value=1200000, step=5000)
-stamp_duty =int( 0.75 * 2870+(house_cost-130000)*0.06)
-
-st.sidebar.text(f"Stamp duty: ${stamp_duty}")
-cash = st.sidebar.number_input('Total cash', value=200000)
+st.sidebar.markdown("## House and loan")
+house_cost = st.sidebar.slider('Cost of house', value=1000000, min_value=300000, max_value=2000000, step=5000)
+state = st.sidebar.selectbox("State:", ["ACT", "Vic", "NSW", "Tas", "Qld", "WA", "SA", "NT"], index=1)
+stamp_duty, info = calc_stamp_duty(state, house_cost)
+if state == "Vic":
+    info = "(discount applied)"
+st.sidebar.text(f"Stamp duty: ${stamp_duty:,} {info}")
+cash = st.sidebar.number_input('Total cash', value=200000, step=5000)
 borrow = st.sidebar.slider("Amount to borrow", value=house_cost + stamp_duty - cash, min_value=500000, max_value=house_cost, step=10000)
 cash_left = cash - house_cost - stamp_duty + borrow 
-st.sidebar.text(f"Cash left: ${cash_left}")
+lvr = borrow/house_cost
+st.sidebar.text(f"Cash left: ${cash_left}     LVR: {lvr*100:.1f}%")
 rate = st.sidebar.slider("Interest rate", value=2.6, min_value=2.0, max_value=5.0, step=.1)
 term = st.sidebar.radio("Term", [20, 25, 30], index=2)
 offset = st.sidebar.radio("Offset", ['Yes', 'No'], index=0)
 
 loan = Loan(borrow, rate, term*12)
-income = st.number_input("Monthly net income", value=15000)
-expenses = st.number_input("Monthly expenses", value=2500)
+income = st.number_input("Monthly net income", value=15000, step=500)
+expenses = st.number_input("Monthly expenses", value=2500, step=100)
 st.text(f"Min. payment: ${int(loan.payment_monthly)}")
 reserve = st.slider("Reserve cash (monthly)", min_value=0, max_value=int(income-expenses-loan.payment_monthly), step=500)
 
 results = scenario2(rate, borrow, income=income, start_cash=cash_left, expenses=expenses, reserve=reserve)
-st.markdown(f"Paid off in {len(results)/12:.1f} years. Total interest paid: ${loan.interest_paid:.0f}")
+st.markdown(f"### Paid off in {len(results)/12:.1f} years. Total interest paid: ${int(results[-1]['interest_paid']):,}")
 fig = make_figure(results)
-
-# st.sidebar.slider()
-
-# wavelength_min = st.sidebar.slider("WL start", min_value=100, max_value=20000, value=1000)
-# wavelength_max = st.sidebar.slider("WL end", min_value=100, max_value=50000, value=10000)
-
-# show_lines = st.sidebar.checkbox("Show lines")
-# spectrum =  st.sidebar.selectbox(
-#     "Plot:",
-#     ['Quiescent', 'Star-Forming', 'M-star', 'other']
-# )
-
-# filters = st.sidebar.multiselect(
-#     "Filters", options=get_all_filters(), default=[], format_func=lambda x: str(x), key='filters'
-# )
-    
-# fig = make_figure(load_spectrum(spectrum), filters)
 
 st.plotly_chart(fig)
 
